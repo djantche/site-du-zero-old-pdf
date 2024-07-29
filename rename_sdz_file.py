fd2f6075e586c02754f696a342beea5ca7ce1499
Djantche Ngamo David Junior <67787883+kage0@users.noreply.github.com>
2024-07-29 14:12:03 +0100

    Update rename_sdz_file.py

--- rename_sdz_file.py
+++ rename_sdz_file.py
@@ -1,6 +1,6 @@
 import os
 
-directory_path = '/home/djantche/Documents/sdz/'
+directory_path = '/home/----/Documents/sdz/'
 
 files = os.listdir(directory_path)
 
@@ -9,6 +9,4 @@ for file_name in files:
         new_name = file_name.split('-', 1)[1].replace('-', ' ').capitalize()
         file_path = os.path.join(directory_path, file_name)
         new_file_path = os.path.join(directory_path, new_name)
-        print(f"new_name: {new_file_path}")
-        print(f"File: {file_path}")
         os.rename(file_path, new_file_path)
